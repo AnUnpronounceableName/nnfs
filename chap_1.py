91f8155a5be601617c14d86c59dfeefe2c70fa99
AnUnpronounceableName <myfirstandlastnamesspelledout@gmail.com>
2022-02-15 20:41:35 -0500

    I have been converted to pre-operator line breaks

--- chap_1.py
+++ chap_1.py
@@ -3,9 +3,10 @@ def ex_1():
     weights = [0.2, 0.8, -0.5]
     bias = 2.0
 
-    output = (inputs[0]*weights[0] +
-              inputs[1]*weights[1] +
-              inputs[2]*weights[2] + bias)
+    output = (inputs[0]*weights[0]
+              + inputs[1]*weights[1]
+              + inputs[2]*weights[2]
+              + bias)
 
     print(output)
 
@@ -21,22 +22,25 @@ def ex_2():
 
     outputs = [
             # Neuron 1
-            inputs[0]*weights1[0] +
-            inputs[1]*weights1[1] +
-            inputs[2]*weights1[2] +
-            inputs[3]*weights1[3] + bias1,
+            inputs[0]*weights1[0]
+            + inputs[1]*weights1[1]
+            + inputs[2]*weights1[2]
+            + inputs[3]*weights1[3]
+            + bias1,
 
             # Neuron 2
-            inputs[0]*weights2[0] +
-            inputs[1]*weights2[1] +
-            inputs[2]*weights2[2] +
-            inputs[3]*weights2[3] + bias2,
+            inputs[0]*weights2[0]
+            + inputs[1]*weights2[1]
+            + inputs[2]*weights2[2]
+            + inputs[3]*weights2[3]
+            + bias2,
 
             # Neuron 3
-            inputs[0]*weights3[0] +
-            inputs[1]*weights3[1] +
-            inputs[2]*weights3[2] +
-            inputs[3]*weights3[3] + bias3]
+            inputs[0]*weights3[0]
+            + inputs[1]*weights3[1]
+            + inputs[2]*weights3[2]
+            + inputs[3]*weights3[3]
+            + bias3]
     
     print(outputs)
 
