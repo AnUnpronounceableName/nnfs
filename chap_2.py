c0b70338fdfaab1906d9dfc6fe1865cd80d97f95
AnUnpronounceableName <myfirstandlastnamesspelledout@gmail.com>
2022-02-21 15:16:30 -0500

    transposing matrices and doing the dot product

--- chap_2.py
+++ chap_2.py
@@ -13,4 +13,32 @@ def ex_2_1():
 
     print(layer_outputs)
 
+def ex_2_p53():
+    import numpy as np
+    a = [1, 2, 3]
+    b = [2, 3, 4]
+
+    a = np.array([a])
+    b = np.array([b]).T
+
+    print(np.dot(a, b))
+
+def ex_2_p54():
+    import numpy as np
+    inputs = [[1.0, 2.0, 3.0, 2.5]
+            , [2.0, 5.0, -1.0, 2.0]
+            , [-1.5, 2.7, 3.3, -0.8]]
+    
+    weights = [[0.2, 0.8, -0.5, 1.0]
+            , [0.5, -0.91, 0.26, -0.5]
+            , [-0.26, -0.27, 0.17, 0.87]]
+
+    biases = [2.0, 3.0, 0.5]
+
+    outputs = np.dot(inputs, np.array(weights).T) + biases
+
+    print(outputs)
+
 ex_2_1()
+ex_2_p53()
+ex_2_p54()
