492fb6aa27ecf9ebb4292892568301816da8a457
AnUnpronounceableName <myfirstandlastnamesspelledout@gmail.com>
2022-02-22 13:11:41 -0500

    still working on this class

--- chap_3.py
+++ chap_3.py
@@ -25,10 +25,13 @@ def ex_3_p60():
 # Dense layer
 class Layer_dense:
 
+    import numpy as np
+    
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
-        pass
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
     
     # Forward pass
     def forward(self, inputs):
